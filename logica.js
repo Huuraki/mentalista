181773e9e2f299506aaa79c4d47dcf8d5014ce01
Huuraki <149612247+Huuraki@users.noreply.github.com>
2023-11-17 12:55:44 -0300

    Update logica.js

--- logica.js
+++ logica.js
@@ -1,13 +1,9 @@
-var button = document.querySelector("button");
-button.addEventListener("click", verifica);
-
-var numeroSecreto = Math.floor(Math.random() * 50) + 1; // Gera um número aleatório de 1 a 50
+var numeroSecreto = geraNumeroSecreto(); // Inicializa o número secreto
 var input = document.querySelector("input");
 
 // Adiciona um listener para o evento "input" que valida o conteúdo do input
 input.addEventListener("input", function () {
-  var valor = parseInt(this.value.replace(/\D/g, '')); // Remove caracteres não numéricos
-  input.focus();
+  var valor = parseInt(this.value.replace(/[^0-9]/g, '')); // Remove caracteres não numéricos
 
   // Limita o valor ao intervalo de 1 a 50
   valor = Math.min(Math.max(valor, 1), 50);
@@ -16,16 +12,22 @@ input.addEventListener("input", function () {
   this.value = valor;
 });
 
+function geraNumeroSecreto() {
+  return Math.floor(Math.random() * 50) + 1;
+}
+
 function verifica() {
   var palpite = parseInt(input.value);
 
   if (palpite === numeroSecreto) {
     alert('Acertou! O número secreto era: ' + numeroSecreto);
+    numeroSecreto = geraNumeroSecreto(); // Gera um novo número secreto
   } else if (palpite > numeroSecreto) {
     alert('Errou... o número secreto é menor que ' + palpite);
   } else if (palpite < numeroSecreto) {
     alert('Errou... o número secreto é maior que ' + palpite);
   }
+
   input.value = "";
   input.focus();
 }
